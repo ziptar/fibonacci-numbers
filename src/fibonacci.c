308d43506a065d6ef11760c907686dba20b28dac
ziptar <33740462+ziptar@users.noreply.github.com>
2020-07-02 02:39:13 +0100

    Update fibonacci.c

--- src/fibonacci.c
+++ src/fibonacci.c
@@ -30,7 +30,13 @@ int main(void) {
 }
 
 int fibonacci(int n) {
-	if (n > 1) return fibonacci(n - 1) + fibonacci(n - 2);
-	if (n == 1) return 1;
-	return 0;
+	if (n > 1) {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	} else if (n == 1) {
+		return 1;
+	} else if (n == 0) {
+		return 0;
+	} else {
+		return -1;
+	}
 }
